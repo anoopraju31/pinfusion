47f7bfc129225711b530edd1344801b9bdd9d3ed
anoopraju31 <anoopraju31@gmail.com>
2023-09-24 14:14:44 +0530

    User Profile Page change

---
+++ src/app/user/[userId]/page.tsx
@@ -0,0 +1,30 @@
+'use client'
+import app from '@/app/firebase'
+import { DocumentData, doc, getDoc, getFirestore } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+
+type ProfilePageProps = {
+	params: {
+		userId: string
+	}
+}
+
+const ProfilePage = ({ params }: ProfilePageProps) => {
+	const [userInfo, setUserInfo] = useState<DocumentData | null>(null)
+	const db = getFirestore(app)
+
+	useEffect(() => {
+		const getUserInfo = async (email: string) => {
+			const userRef = doc(db, 'user', email)
+			const userSnap = await getDoc(userRef)
+
+			setUserInfo(userSnap.exists() ? userSnap.data() : null)
+		}
+
+		if (params) getUserInfo(params.userId.replace('%40', '@'))
+	}, [params, db])
+
+	return <div>ProfilePage</div>
+}
+
+export default ProfilePage
