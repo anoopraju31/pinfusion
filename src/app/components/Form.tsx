5f2fc5cca09e554e929aa347759df1e0934ecc22
anoopraju31 <anoopraju31@gmail.com>
2023-09-24 15:17:05 +0530

    Image Upload Component - initial

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { ImageUpload } from '.'
 
 const Form = () => {
-	return <div>Form</div>
+	return (
+		<section className='p-16 bg-white rounded-2xl'>
+			{/* Loading */}
+
+			<form className='grid grid-cols-1 lg:grid-cols-3 gap-10'>
+				{/* Image Upload */}
+				<ImageUpload />
+
+				{/* User Inputs */}
+			</form>
+		</section>
+	)
 }
 
 export default Form
