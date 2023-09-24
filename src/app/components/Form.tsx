fa508c213214cc4fc99cc88c2d2d78d191d1bf72
anoopraju31 <anoopraju31@gmail.com>
2023-09-24 18:50:24 +0530

    Input Field Component - styledl

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import { ImageUpload, InputField } from '.'
 
+type FormType = {
+	title: string
+	description: string
+	destinationLink: string
+	image: File | null
+}
+
 const Form = () => {
+	const [form, setForm] = useState<FormType>({
+		title: '',
+		description: '',
+		destinationLink: '',
+		image: null,
+	})
+
+	const handleInputChange = (
+		fieldName: 'title' | 'description' | 'destinationLink' | 'image',
+		e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
+	) => {
+		if (fieldName === 'image') {
+			const target = e.target as HTMLInputElement
+			const files = Array.from(target.files as FileList)
+			setForm((prevForm) => ({ ...prevForm, [fieldName]: files[0] }))
+		} else setForm((prevForm) => ({ ...prevForm, [fieldName]: e.target.value }))
+	}
+
 	return (
 		<section className='p-16 bg-white rounded-2xl'>
 			{/* Loading */}
@@ -12,7 +39,36 @@ const Form = () => {
 
 				{/* User Inputs */}
 				<div className='col-span-2'>
-					<InputField />
+					<InputField
+						labelName='Pin Title'
+						id='title'
+						placeholder='Add you title'
+						inputType='text'
+						value={form.title}
+						handleChange={(e) => handleInputChange('title', e)}
+						styles='text-5xl font-extrabold'
+					/>
+
+					<InputField
+						labelName='Description'
+						id='description'
+						placeholder='Tell everyone what your pin is about'
+						isTextArea
+						value={form.description}
+						row={5}
+						handleChange={(e) => handleInputChange('description', e)}
+						styles='text-base max-h-24 overflow-auto !font-normal placeholder:font-medium'
+					/>
+
+					<InputField
+						labelName='Destination Link'
+						id='destination-link'
+						placeholder='Add a Destination Link'
+						inputType='url'
+						value={form.destinationLink}
+						handleChange={(e) => handleInputChange('destinationLink', e)}
+						styles='text-base !font-normal placeholder:font-light'
+					/>
 				</div>
 			</form>
 		</section>
--- src/app/components/InputField.tsx
+++ src/app/components/InputField.tsx
@@ -1,7 +1,60 @@
-import React from 'react'
+'use client'
 
-const InputField = () => {
-	return <div>InputField</div>
+import { useState } from 'react'
+
+type InputFieldPrps = {
+	labelName: string
+	id: string
+	placeholder: string
+	inputType?: string
+	isTextArea?: boolean
+	row?: number
+	value: string
+	handleChange: (
+		e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
+	) => void
+	styles?: string
+}
+
+const InputField = ({
+	labelName,
+	id,
+	placeholder,
+	inputType,
+	isTextArea,
+	row,
+	value,
+	handleChange,
+	styles,
+}: InputFieldPrps) => {
+	return (
+		<div className='group'>
+			{isTextArea ? (
+				<textarea
+					id={id}
+					required
+					value={value}
+					rows={row}
+					placeholder={placeholder}
+					onChange={handleChange}
+					className={`w-full outline-none my-8 py-2 border-b-2 border-b-gray-300 font-roboto text-zinc-700 placeholder:text-zinc-500 !focus:shadow-[var(--focus-shadow)] focus:border-blue-500 ${styles}`}
+				/>
+			) : (
+				<input
+					id={id}
+					type={inputType}
+					required
+					value={value}
+					placeholder={placeholder}
+					onChange={handleChange}
+					className={`w-full my-8 py-2 border-b-2 border-b-gray-300 font-roboto text-zinc-700 placeholder:text-zinc-500 !focus:shadow-[var(--focus-shadow)] focus:border-blue-500 outline-none ${styles}`}
+				/>
+			)}
+			<label htmlFor={id} className='hidden'>
+				{labelName}
+			</label>
+		</div>
+	)
 }
 
 export default InputField
