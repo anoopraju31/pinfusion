96a6b82a3d0fb93979c38baa7956dd426e6e170a
anoopraju31 <anoopraju31@gmail.com>
2023-09-24 15:10:40 +0530

    Form Component - initial

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Form } from '../components'
 
 const CreatePinPage = () => {
-	return <div>CreatePinPage</div>
+	return (
+		<main className='min-h-screen p-8 px-[10px] md:px-[160px] bg-[#e9e9e9]'>
+			<Form />
+		</main>
+	)
 }
 
 export default CreatePinPage
