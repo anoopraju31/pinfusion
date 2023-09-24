f39f642f13d2c7e9be7f5fde2f298c62211094e1
anoopraju31 <anoopraju31@gmail.com>
2023-09-24 12:57:34 +0530

    Firebase App Initialized

---
+++ src/app/firebase/index.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: process.env.FIREBASE_API_KEY as string,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN as string,
+	projectId: process.env.FIREBASE_PROJECT_ID as string,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET as string,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER__ID as string,
+	appId: process.env.FIREBASE_APP_ID as string,
+	measurementId: process.env.FIREBASE_MEASUREMENT_ID as string,
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
