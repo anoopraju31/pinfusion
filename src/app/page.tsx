5f2b23d5208f8970231cc93f486e183605099232
anoopraju31 <anoopraju31@gmail.com>
2023-09-25 18:16:25 +0530

    Home Page - Styled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,38 @@
-import Image from 'next/image'
+'use client'
+
+import { useEffect, useState } from 'react'
+import {
+	DocumentData,
+	collection,
+	getDocs,
+	getFirestore,
+	query,
+} from 'firebase/firestore'
+import { PinsList } from './components'
+import app from './firebase'
 
 export default function Home() {
-	return <main>Home Page</main>
+	const [pins, setPins] = useState<DocumentData>([])
+	const db = getFirestore(app)
+
+	useEffect(() => {
+		const getAllPins = async () => {
+			const pinsQuery = query(collection(db, 'posts'))
+			const querySnapshot = await getDocs(pinsQuery)
+			let pins: DocumentData[] = []
+
+			querySnapshot.forEach((doc) => {
+				pins.push(doc.data())
+			})
+
+			setPins(pins)
+		}
+
+		getAllPins()
+	}, [db])
+	return (
+		<main>
+			<PinsList pins={pins as DocumentData[]} />
+		</main>
+	)
 }
--- src/app/pins/[pinId]/page.tsx
+++ src/app/pins/[pinId]/page.tsx
@@ -1,11 +1,12 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { PinImage } from '@/app/components'
-import { HiArrowSmallLeft } from 'react-icons/hi2'
 import { useRouter } from 'next/navigation'
 import { DocumentData, doc, getDoc, getFirestore } from 'firebase/firestore'
+import { HiArrowSmallLeft } from 'react-icons/hi2'
 import app from '@/app/firebase'
+import { PinImage, UserTag } from '@/app/components'
+import Image from 'next/image'
 
 type PinDetailsPageProps = {
 	params: {
@@ -38,9 +39,27 @@ const PinDeatailsPage = ({ params }: PinDetailsPageProps) => {
 				/>
 				<div className='grid grid-cols-1 lg:grid-cols-2 md:gap-10 shadow-lg rounded-2xl p-3 md:p-7 lg:p-12 xl:pd-16 '>
 					{/* Pin Image */}
-					<PinImage />
+					<div>
+						<Image
+							src={pinDetails.image}
+							alt={pinDetails.title}
+							width={500}
+							height={600}
+							className='rounded-2xl'
+						/>
+					</div>
+
 					{/* Pin Details */}
-					{JSON.stringify(pinDetails)}
+					<div>
+						<h2 className='text-[30px] font-bold mb-10'>{pinDetails.title}</h2>
+						<UserTag />
+						<h2 className='mt-10'>{pinDetails.desc}</h2>
+						<button
+							className='p-2 bg-[#e9e9e9] px-5 text-[23px] mt-10 rounded-full hover:scale-105 transition-all'
+							onClick={() => window.open(pinDetails.link)}>
+							Open Url
+						</button>
+					</div>
 				</div>
 			</main>
 		)
