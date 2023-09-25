64d7daa37e9a03c404a98fa01c631d0d22a23bfc
anoopraju31 <anoopraju31@gmail.com>
2023-09-25 17:07:40 +0530

    Pin Item Conponent styled

--- src/app/components/PinItem.tsx
+++ src/app/components/PinItem.tsx
@@ -1,7 +1,35 @@
+import { DocumentData } from 'firebase/firestore'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { UserTag } from '.'
 
-const PinItem = () => {
-	return <div>PinItem</div>
+type PinItemType = {
+	pin: DocumentData
+}
+
+const PinItem = ({ pin }: PinItemType) => {
+	return (
+		<div className=''>
+			<Link href='/'>
+				<div className='relative before:absolute before:h-full before:w-full before:rounded-3xl before:z-10 hover:before:bg-gray-600 before:opacity-50 cursor-pointer'>
+					<Image
+						src={pin.image}
+						alt={pin.title}
+						width={500}
+						height={500}
+						className='rounded-3xl cursor-pointer relative z-0'
+					/>
+				</div>
+			</Link>
+
+			<h3 className='font-bold text-[18px] mb-1 mt-2 line-clamp-2'>
+				{pin.title}
+			</h3>
+
+			<UserTag />
+		</div>
+	)
 }
 
 export default PinItem
--- src/app/components/PinsList.tsx
+++ src/app/components/PinsList.tsx
@@ -10,7 +10,7 @@ const PinsList = ({ pins }: PinsListProps) => {
 	return (
 		<section className='mt-7 px-2 md:px-5 columns-2 md:columns-3 lg:columns-4 mb-4 xl:columns-5 space-y-6 mx-auto'>
 			{pins.map((item, idx) => (
-				<PinItem key={idx} />
+				<PinItem key={idx} pin={item} />
 			))}
 		</section>
 	)
